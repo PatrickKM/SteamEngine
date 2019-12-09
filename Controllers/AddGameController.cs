791d8caa528983e19adbdd1853fd4f54c8a4312b
a1d0x <jphansen8000@gmail.com>
2019-12-09 10:40:28 +0100

    Add Custom Game BACKEND
    Add Custom Game BACKEND

---
+++ Controllers/AddGameController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exercise01.Controllers
+{
+    public class AddGameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormCollection formCollection)
+        {
+            string appId = "https://steamcdn-a.akamaihd.net/steam/apps/" + formCollection["AppId"] + "/header.jpg";
+            string name = formCollection["Name"];
+            string description = formCollection["Description"];
+
+            ViewData["AppId"] = appId;
+            ViewData["Name"] = name;
+            ViewData["Description"] = description;
+
+            return View();
+        }
+    }
+}
