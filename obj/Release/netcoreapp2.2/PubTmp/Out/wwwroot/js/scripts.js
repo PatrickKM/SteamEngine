0f7fc5f0408751f5715fe764b0740293ea6b5874
Patrick Koo <patrickkoomadsen@gmail.com>
2019-12-11 12:06:38 +0100

    CSS

--- obj/Release/netcoreapp2.2/PubTmp/Out/wwwroot/js/scripts.js
+++ obj/Release/netcoreapp2.2/PubTmp/Out/wwwroot/js/scripts.js
@@ -1,29 +1,26 @@
-﻿let url = 'https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=84B03B60EEC3A7DF3D578C07814939E5&include_appinfo=1&include_played_free_games=1';
-let steamID = 'steamid=76561197960434622';
-const proxyurl = "https://cors-anywhere.herokuapp.com/";
-
-// document.getElementById("valueSteamID").value = steamID;
-
-// STEAM ID: steamid=76561198025683997 (Jesper)
-// STEAM ID: steamid=76561197960434622 (STEAM)
+﻿// STEAM ID: steamid=76561198025683997 (Jesper) 
+// STEAM ID: steamid=76561197960434622 (STEAM) 
 // STEAM ID: steamid=76561198001921578 (Patrick)
+// STEAM ID: steamid=76561198039364734 (Catie)
 
-let container = document.getElementById("moviesRoot");
+function steamIdFinder() {
+    let url = 'https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=84B03B60EEC3A7DF3D578C07814939E5&include_appinfo=1&include_played_free_games=1';
+    let steamID = document.getElementById("steamID").value;
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    let container = document.getElementById("gameRoot");
+    let fetchUrl = url + '&steamid=' + steamID;
 
-let fetchUrl = url + '&' + steamID;
-fetch(proxyurl + fetchUrl)
-    .then(response => {
-        return response.json();
-    })
-    .then(steamID => {
-        console.log(steamID);
+
+    fetch(proxyurl + fetchUrl)
+        .then(response => {
+            return response.json();
+        })
+        .then(steamID => {
+            console.log(steamID);
 
             const movieSection = document.createElement('section');
             movieSection.setAttribute('class', 'column');
-
-            const idInput = document.createElement('input');
-            idInput.setAttribute("id", "valueSteamID");
-            idInput.setAttribute("type", "text");
+            movieSection.setAttribute('id', 'gameSection');
 
             const title = document.createElement('p');
             const playtime = document.createElement('p');
@@ -32,15 +29,23 @@ fetch(proxyurl + fetchUrl)
             let steamName = steamID.response.games.sort(function (a, b) { return 0.5 - Math.random() });
 
             title.textContent = steamName[0].name;
-            playtime.textContent = steamName[0].playtime_forever;
-            banner.setAttribute("src", "http://media.steampowered.com/steamcommunity/public/images/apps/" + steamName[0].appid + "/" + steamName[0].img_logo_url + ".jpg");
+            playtime.textContent = 'Amount of playtime on game: ' + steamName[0].playtime_forever + ' minutes';
+            banner.setAttribute("src", "https://steamcdn-a.akamaihd.net/steam/apps/" + steamName[0].appid + "/header.jpg");
+            banner.setAttribute("class", "bannerimg");
 
             movieSection.appendChild(title);
             movieSection.appendChild(playtime);
             movieSection.appendChild(banner);
             container.appendChild(movieSection);
-    })
+        })
 
-    .catch(function (err) {
-        console.log('error: ' + err);
-    });
+        .catch(function (err) {
+            console.log('error: ' + err);
+        });
+}
+
+function resetGames() {
+    let removeGames = document.querySelectorAll("[id='gameSection']");
+    for (let i = 0; i < removeGames.length; i++)
+        removeGames[i].parentNode.removeChild(removeGames[i]);
+}
